efb8ef8e3513a96c28e5b1971183be4ea546361f
AhmedX404 <ahmmedd606@gmail.com>
2024-10-19 23:22:33 +0300

    fix status isuue

--- resources/views/enter-room.blade.php
+++ resources/views/enter-room.blade.php
@@ -26,19 +26,27 @@
                         <h3>Access Your Room</h3>
                     </div>
                     <div class="card-body">
-                        @if($room_object->status)
-                            <form action="{{route('enter-room-post', ['room' => $room])}}" method="POST">
-                                @csrf
-                                <div class="form-group">
-                                    <input type="password" class="form-control text-center" id="password" name="password" required placeholder="Enter Room Password">
-                                </div>
-                                <button type="submit" class="btn btn-primary btn-block">Enter</button>
-                            </form>
-                        @else
-                            <div class="alert alert-danger text-center">
-                                This room is currently disabled.
+{{--                        @if($room_object->status)--}}
+{{--                            <form action="{{route('enter-room-post', ['room' => $room])}}" method="POST">--}}
+{{--                                @csrf--}}
+{{--                                <div class="form-group">--}}
+{{--                                    <input type="password" class="form-control text-center" id="password" name="password" required placeholder="Enter Room Password">--}}
+{{--                                </div>--}}
+{{--                                <button type="submit" class="btn btn-primary btn-block">Enter</button>--}}
+{{--                            </form>--}}
+{{--                        @else--}}
+{{--                            <div class="alert alert-danger text-center">--}}
+{{--                                This room is currently disabled.--}}
+{{--                            </div>--}}
+{{--                        @endif--}}
+
+                        <form action="{{route('enter-room-post', ['room' => $room])}}" method="POST">
+                            @csrf
+                            <div class="form-group">
+                                <input type="password" class="form-control text-center" id="password" name="password" required placeholder="Enter Room Password">
                             </div>
-                        @endif
+                            <button type="submit" class="btn btn-primary btn-block">Enter</button>
+                        </form>
                     </div>
                 </div>
             </div>
